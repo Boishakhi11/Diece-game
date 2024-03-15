79dfd58be772246f48c3d57b3d269ff86cbf461a
Boishakhi11 <bgmukta11@gmail.com>
2024-03-15 11:31:31 +0100

    complete

---
+++ index.js
@@ -0,0 +1,30 @@
+let randomNumber1= Math.floor(Math.random() * 6) + 1;
+
+var randomImage = "dice" + randomNumber1 + ".png";
+
+var randomImageSources= "images/" + randomImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSources);
+
+
+var randomNumber2= Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2= "images/dice"+ randomNumber2 + ".png" ;
+
+document.querySelectorAll("img") [1].setAttribute("src", randomImageSource2);
+
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML= "Player 1 Wins!!";
+}
+
+else if (randomNumber1 === randomNumber2)
+{
+    document.querySelector("h1").innerHTML="Draw!!";
+}
+  else
+{
+    document.querySelector("h1").innerHTML="Player 2 Wins!!";
+}
